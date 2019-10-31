61b44b00f232ba24d3bbc59f7f2ba03a1bb721cd
Maxime-Beaufils <thelimier@gmail.com>
2019-10-31 11:12:06 +0100

    mise en place de l'API dark sky via vueX

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,28 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <b-button type="is-info" @click="getWeatherData">Get weather</b-button>
+    <p>This component’s code is in {{ weather_data }}</p>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+import { mapActions } from 'vuex';
+import { mapGetters } from 'vuex';
+export default {
+  data() {
+    return {
+      
     }
-  }
+  },
+  methods: {
+    ...mapActions(['getWeatherData']),
+  },
+  computed: {
+    ...mapGetters(['weather_data']),
+  },
 }
+</script>
+
+<style>
+
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,37 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    weather_data: {ciel : "pluie"},
+    coordonate: [0.370679, 49.182863],
+    dark_api_url: "https://api.darksky.net/forecast"
   },
-  mutations: {
+  getters : {
+    weather_data : state => {
+      return state.weather_data;
+    }
   },
-  actions: {
+  mutations : {
+    setCoordonate : (state, coor) => {
+      state.coordonate = coor
+    },
+    setWeatherData : (state, data) => {
+      state.weather_data = data 
+    }
+  },
+  actions : {
+    getCoordonate : async (context) => {
+      context
+    },
+
+    getWeatherData :  async (context) => {
+      let data = await Axios.get(context.state.dark_api_url + '/' + `${process.env.VUE_APP_SKY}` + '/' + context.state.coordonate[0] + '/' + context.state.coordonate[1])
+      context.commit('setWeatherData', data)
+    }
   },
   modules: {
   }
